8f049b5d137c55af541a794a20076496ed2ce555
hamletmillqvist <hamlet.millqvist@protonmail.ch>
2024-05-26 17:37:22 +0200

    Created dev environment using react + material UI with hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as Material from "@mui/material"
+import * as Icon from "@mui/icons-material"
+import {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [dialogOpen, setDialogOpen] = useState<boolean>(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const onClick = () => {
+        setDialogOpen(true);
+    }
+
+    return (
+        <div className="app">
+            <Material.Button variant="contained" onClick={onClick}>
+                Click me!
+            </Material.Button>
+            <Material.Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+                <Material.DialogTitle> You clicked me! <Icon.EmojiEmotions/></Material.DialogTitle>
+                <Material.DialogContent> This is some more content inside the dialog.</Material.DialogContent>
+            </Material.Dialog>
+        </div>
+    )
+};
 
 export default App
